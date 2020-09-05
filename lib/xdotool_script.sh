488b21e6debd7240e20d35f359a122627e005574
Antoni Baum <antoni.baum@protonmail.com>
2020-09-05 02:18:20 +0200

    Fix

--- lib/xdotool_script.sh
+++ lib/xdotool_script.sh
@@ -70,6 +70,7 @@ do
         echo "Accept Steam EULA..."
         xdotool mousemove 660 555
         xdotool click 1 mousemove 0 0
+        break
     elif compare_current_screen "$HOME/image_specimens/steam_eula_new_474_351_403_161.png" "474x351+403+161"; then
         echo "Accept update..."
         xdotool mousemove 715 535
